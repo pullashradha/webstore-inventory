71016d453e266ed1d8416bcf4994311967dfd4b4
Shradha Pulla <pullashradha@gmail.com>
2016-09-21 11:23:29 -0700

    Create basic files, create product model, no identity relationships

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStoreInventory.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+		public DbSet<ApplicationProduct> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WebStoreInventory;integrated security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/ApplicationProduct.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebStoreInventory.Models
+{
+    [Table("Products")]
+    public class ApplicationProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public ApplicationProduct(string name, int quantity, int price, int id = 0)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Id = id;
+        }
+
+        public ApplicationProduct() { }
+    }
+}
